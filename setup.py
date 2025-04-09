5274d85fbab5eb31771fb5baddfd83e29b44a69c
shizeying <w741069229@gmail.com>
2025-04-10 00:47:59 +0800

    chore: 更新版本号到 0.1.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mcp-coze-server",
-    version="0.1.10",
+    version="0.1.12",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
